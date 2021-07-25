e2efd10f20a568f58cccd4ec1b617c303a5eb70d
zzsyjl <287683119@qq.com>
2021-07-25 17:32:35 +0800

    写好dfs, 但是test错误

--- chap04树与图/01route_between_nodes.py
+++ chap04树与图/01route_between_nodes.py
@@ -10,7 +10,7 @@ from collections import deque
 真正的数据结构:
 graph = {
         "A": ["B", "C"],
-		...
+        ...
         "R": ["P", "Q"],
     }
 对有向图来说, 这样最简单得搞个字典嵌套列表就行了. 
@@ -21,29 +21,46 @@ graph = {
 
 思路:
 这要在图上做搜索, 就是深度优先和广度优先嘛. dfs和bfs. 
+
+结论:
+果然bfs就是用队列来实现的. 没有问题!
+
+dfs:
+这里面
 """
 
 def has_route_bfs(graph, node_a, node_b):
-	deque_1 = deque([node_a])
-	visited = {node_a}
-	if node_b in deque_1:
-		return True
-	while deque_1:
-		# 进队列时, 验证是否到b点, 以及对visited更新
-		# 出队列时, 只是找到其下家, 然后这个就可以丢弃了
-		popped = deque_1.popleft()
-		children = graph[popped]
-		if node_b in children:
-			return True
-		for child in children:
-			if child not in visited:
-				# 没由访问过, 加入访问过的集合, 同时加到队列里
-				visited.add(child)
-				deque_1.append(child)
-	return False
+    deque_1 = deque([node_a])
+    visited = {node_a}
+    if node_b in deque_1:
+        return True
+    while deque_1:
+        # 进队列时, 验证是否到b点, 以及对visited更新
+        # 出队列时, 只是找到其下家, 然后这个就可以丢弃了
+        popped = deque_1.popleft()
+        children = graph[popped]
+        if node_b in children:
+            return True
+        for child in children:
+            if child not in visited:
+                # 没由访问过, 加入访问过的集合, 同时加到队列里
+                visited.add(child)
+                deque_1.append(child)
+    return False
+
+def has_route_dfs(graph, node_a, node_b, visited=set()):
+    if node_a == node_b:
+        return True
+
+    children = graph[node_a]
+    for child in children:
+        if has_route_dfs(graph, child, node_b):
+            return True
+    return False
 
 
-		
+
+        
 
 
 
@@ -70,7 +87,7 @@ class Test(unittest.TestCase):
 
     tests = [
         ("A", "L", True),
-		("A", "A", True),
+        ("A", "A", True),
         ("A", "B", True),
         ("H", "K", True),
         ("L", "D", True),
@@ -91,5 +108,10 @@ class Test(unittest.TestCase):
             actual = has_route_bfs(self.graph, start, end)
             assert actual == expected
 
+    def test_is_route_dfs(self):
+        for [start, end, expected] in self.tests:
+            actual = has_route_dfs(self.graph, start, end)
+            assert actual == expected
+
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
